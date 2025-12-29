c98ab52b1583e5f472d6e80b6369e985792a7500
priyansi032005 <priyansi032005@gmail.com>
2025-12-29 23:48:11 +0530

    fix the error of backend API

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,21 +1,7 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import (
     home,
     login_page,
@@ -32,19 +18,28 @@ from .views import (
 )
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
+
     path("", home, name="home"),
     path("login/", login_page, name="login"),
     path("register/", register_page, name="register"),
     path("logout/", logout_page, name="logout"),
-
     path("doctor/appointments/", appointment_list, name="appointment_list"),
-
     path("doctor/dashboard/", doctor_dashboard, name="doctor_dashboard"),
     path("doctor/prescriptions/", prescription_list, name="prescription_list"),
     path("doctor/prescriptions/<int:prescription_id>/pdf/", prescriptionPDF_view, name="prescription_pdf"),
     path("doctor/sidebar/", doctor_sidebar, name="doctor_sidebar"),
     path("doctor/settings/", doctor_settings, name="doctor_settings"),
     path("doctor/patientlist/", doctor_patient_list, name="doctor_patient_list"),
-
     path("patient/appointments/", patient_appointment_list, name="patient_appointment_list"),
+
+    path("api/", include("accounts.urls")),
+    path("api/appointments/", include("appointments.urls")),
+    path("api/doctors/", include("doctors.urls")),
+    path("api/patient/", include("patients.urls")),
+    path("api/prescriptions/", include("prescriptions.urls")),
+    path("api/medical_files/", include("medical_files.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
